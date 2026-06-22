4fbe647ed1cac7c9e6b2101e8e3520baf37795f8
Michael Chadwick <michael.chadwick@gmail.com>
2026-06-22 13:28:18 -0700

    fix an unneeded warning from vueuse until it updates past 14.3.0

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,18 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rolldownOptions: {
+      // TODO: Remove when updating vueuse beyond v14.3.0
+      onwarn(warning, warn) {
+        if (warning.code === 'INVALID_ANNOTATION') {
+          return
+        }
+
+        warn(warning)
+      },
+    },
+  },
   plugins: [vue(), vueDevTools()],
   base: './',
   resolve: {
