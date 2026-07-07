b59f11bc9d334f1d87b8f866216d66c2fddde4e8
Michael Chadwick <michael.chadwick@gmail.com>
2026-07-07 13:22:11 -0700

    mock pinia in Fretboard unit test

--- src/components/__tests__/Fretboard.spec.ts
+++ src/components/__tests__/Fretboard.spec.ts
@@ -1,9 +1,16 @@
-import { describe, it, expect } from 'vitest'
-
+import { describe, it, expect, beforeEach } from 'vitest'
+import { setActivePinia, createPinia } from 'pinia'
 import { mount } from '@vue/test-utils'
 import Fretboard from '../Fretboard.vue'
 
 describe('Fretboard', () => {
+  beforeEach(() => {
+    // creates a fresh pinia and makes it active
+    // so it's automatically picked up by any useStore() call
+    // without having to pass it to it: `useStore(pinia)`
+    setActivePinia(createPinia())
+  })
+
   it('renders properly', () => {
     const wrapper = mount(Fretboard)
     const fretNumbers = Array(25)
